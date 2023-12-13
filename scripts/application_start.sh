ebde1808b7b6b40a7eb87735b616a0f13ec66ec2
rutwiksquareops <rutwik@squareops.com>
2023-12-14 00:14:56 +0530

    modified shell scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script to start the web server after the new version has been installed
 
-service apache2 start
+systemctl start apache2
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Script to stop the web server before installing the new version
 
-service apache2 stop
+systemctl stop apache2
+
