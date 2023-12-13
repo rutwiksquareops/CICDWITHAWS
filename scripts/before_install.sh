14d62daf7440b86d3e3ea16e5e9d344309aa2111
rutwiksquareops <rutwik@squareops.com>
2023-12-14 01:15:14 +0530

    added sed

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,4 +2,4 @@
 # Script to stop the web server before installing the new version
 
 systemctl stop apache2
-sudo sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/wordpress|' /etc/apache2/sites-available/000-default.conf
+sudo sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/html/wordpress|' /etc/apache2/sites-available/000-default.conf
